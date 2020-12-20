5f77e7f1b0f5b996ec736b8ae5b0ab99f86c7c6f
Alexis Quinntero <alexis_quintero@hotmail.com.ar>
2020-12-20 11:03:51 -0300

    Working derivation

--- asus-nb-ctrl.nix
+++ asus-nb-ctrl.nix
@@ -1,16 +1,20 @@
-{ stdenv, pkgs }:
+{ lib, rustPlatform, pkgs }:
 
-stdenv.mkDerivation rec {
+rustPlatform.buildRustPackage rec {
   name = "ASUS-NB-Ctrl";
+
   src = builtins.fetchGit {
     url = "https://gitlab.com/asus-linux/asus-nb-ctrl";
     ref = "next";
     rev = "b496139063b6d83104c4e2908b3c2c8a5f06b926";
   };
 
-  buildInputs = with pkgs; [ cargo ];
+  cargoSha256 = "sha256:1wrbhj3l1gqiw00nwkb138fwr7whd0inlhg6n6fg7qv23pjcbj0j";
 
-  meta = with stdenv.lib; {
+  nativeBuildInputs = with pkgs; [ pkg-config ];
+  buildInputs = with pkgs; [ dbus udev ];
+
+  meta = with lib; {
     description = "asusd is a utility for Linux to control many aspects of various ASUS laptops but can also be used with non-asus laptops with reduced features.";
     homepage = "https://gitlab.com/asus-linux/asus-nb-ctrl";
     platforms = platforms.linux;
