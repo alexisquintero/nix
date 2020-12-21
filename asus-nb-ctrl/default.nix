fb83c762cb2f282a56e1334993169e1973734b16
Alexis Quinntero <alexis_quintero@hotmail.com.ar>
2020-12-21 11:45:05 -0300

    Service wirking, needs manual start

--- asus-nb-ctrl/default.nix
+++ asus-nb-ctrl/default.nix
@@ -13,50 +13,48 @@ let
     };
 
     makeFlags = [
-      "DESTDIR=${placeholder "out"}"
-      ];
+      "prefix=${placeholder "out"}"
+    ];
 
-      cargoSha256 = "sha256:1wrbhj3l1gqiw00nwkb138fwr7whd0inlhg6n6fg7qv23pjcbj0j";
+    cargoSha256 = "sha256:1wrbhj3l1gqiw00nwkb138fwr7whd0inlhg6n6fg7qv23pjcbj0j";
 
-      nativeBuildInputs = with pkgs; [ pkg-config ];
-      buildInputs = with pkgs; [ dbus udev ];
+    nativeBuildInputs = with pkgs; [ pkg-config ];
+    buildInputs = with pkgs; [ dbus udev ];
 
-  # patchPhase = ''
-  #   substituteInPlace Makefile --replace \
-  #   "/etc/asusd/" \
-  #   $out/etc/asusd
-  # '';
+    patchPhase = ''
+      substituteInPlace Makefile --replace \
+      "/etc/asusd/" \
+      $out/etc/asusd/
+    '';
 
-  # postInstall = ''
-  #   mkdir -p /etc/asusd
-  #   cp $out/etc/asusd/asusd-ledmodes.toml /etc/asusd/
-  # '';
+    configurePhase = null;
+    buildPhase = null;
+    checkPhase = null;
+    installPhase = null;
 
-  configurePhase = null;
-  buildPhase = null;
-  checkPhase = null;
-  installPhase = null;
-
-  meta = with lib; {
-    description = "asusd is a utility for Linux to control many aspects of various ASUS laptops but can also be used with non-asus laptops with reduced features.";
-    homepage = "https://gitlab.com/asus-linux/asus-nb-ctrl";
-    platforms = platforms.linux;
+    meta = with lib; {
+      description = "asusd is a utility for Linux to control many aspects of various ASUS laptops but can also be used with non-asus laptops with reduced features.";
+      homepage = "https://gitlab.com/asus-linux/asus-nb-ctrl";
+      platforms = platforms.linux;
+    };
   };
-};
 
 in {
+
+  environment.systemPackages = [ asus-nb-ctrl ];
 
   systemd.services = {
     asusd = {
       serviceConfig = {
-        ExecStart = "${asus-nb-ctrl}/usr/bin/asusd";
+        ExecStart = "${asus-nb-ctrl}/bin/asusd";
+        Restart = "always";
       };
       enable = true;
     };
 
     asus-notify = {
       serviceConfig = {
-        ExecStart = "${asus-nb-ctrl}/usr/bin/asus-notify";
+        ExecStart = "${asus-nb-ctrl}/bin/asus-notify";
       };
       enable = true;
     };
