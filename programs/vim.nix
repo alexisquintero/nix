349526ba92aec8b199899d5c3962660051fadb5d
Alexis Quintero <alexis_quintero@hotmail.com.ar>
2024-05-25 12:55:58 +0900

    Don't track vim plugins in nix

--- programs/vim.nix
+++ programs/vim.nix
@@ -1,47 +1,6 @@
 {  pkgs, vim-config, ... }:
 
-let
-
-  plugins = with pkgs.vimPlugins; [
-    cmp-buffer
-    cmp-nvim-lsp
-    cmp-nvim-lsp-signature-help
-    cmp_luasnip
-    comment-nvim
-    diffview-nvim
-    fidget-nvim
-    flash-nvim
-    gitsigns-nvim
-    haskell-tools-nvim
-    luasnip
-    nvim-cmp
-    nvim-lspconfig
-    nvim-metals
-    nvim-treesitter-textobjects
-    nvim-treesitter.withAllGrammars
-    plantuml-syntax
-    plenary-nvim
-    rust-tools-nvim
-    substrata-nvim
-    targets-vim
-    telescope-fzf-native-nvim
-    telescope-nvim
-    telescope-ui-select-nvim
-    vim-cool
-    vim-fireplace
-    vim-fugitive
-    vim-nix
-    vim-printer
-    vim-rhubarb
-    vim-sandwich
-    vim-scala
-    vim-sexp
-    vim-unimpaired
-  ];
-
-in
 {
-
   xdg.configFile."nvim" = {
     recursive = true;
     source = "${vim-config}";
@@ -49,9 +8,9 @@ in
 
   programs.neovim = {
     enable = true;
-    plugins = plugins;
     extraPackages = with pkgs; [
       nixd
+      cmake
     ];
     vimAlias = true;
     vimdiffAlias = true;
